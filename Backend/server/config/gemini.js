808a0831e0e724d7472a55524bbbd34cc2c89557
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2026-02-07 12:04:59 +0530

    changed model

--- Backend/server/config/gemini.js
+++ Backend/server/config/gemini.js
@@ -18,7 +18,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash",
+    model: "gemini-1.5-flash-latest",
   });
   
   const generationConfig = {
